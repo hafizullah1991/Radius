f37d2bff3b6cd1f15ec2b07312b2207710f809ef
Hafiz Hamdard <ahmadshakhi2020@gmail.com>
2024-01-15 13:27:49 -0800

    all in one project

--- operators2/Program.cs
+++ operators2/Program.cs
@@ -4,8 +4,32 @@
     {
         static void Main(string[] args)
         {
+
+            int a;
+            int b;
+            Console.WriteLine("enter Number1: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter Number2: ");
+            b = int.Parse(Console.ReadLine());
+
+
+            int total = a + b;
+            Console.WriteLine($"{a} + {b} is  {total}\n");
+
+            //int quotient = a / b;
+            int Quotient = a / b;
+            Console.WriteLine($"{a} / {b} is {Quotient}\n");
+
+
+            //int remainder = a % b;
+            int remainder = a % b;
+            Console.WriteLine($"{a}/{b} is remainder {remainder}\n");
+
+
             Console.WriteLine("Whaat is the area of your circle: ");
             double radius = double.Parse(Console.ReadLine());
+
 
             double sum= AreaOfCircle(radius);
             Console.WriteLine($"The area of circle with radius of {radius} is {sum}");
