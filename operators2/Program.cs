ee0a510dcae129a4de6865834fe541d6cb90b1d2
Hafiz Hamdard <ahmadshakhi2020@gmail.com>
2024-01-15 09:30:17 -0800

    Radius

---
+++ operators2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace operators2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Whaat is the area of your circle: ");
+            double radius = double.Parse(Console.ReadLine());
+
+            double sum= AreaOfCircle(radius);
+            Console.WriteLine($"The area of circle with radius of {radius} is {sum}");
+        }
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+    }
+}
